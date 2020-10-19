edf40837302cb917cd58580022c405788710b1ab
hhutku <hhutku.wa@gmail.com>
2020-10-19 12:44:06 -0700

    new features  added

--- script.js
+++ script.js
@@ -7,41 +7,59 @@ function writePassword() {
   var passwordText = document.querySelector("#password");
   var passW;
   var str;
-  
+
   function generatePassword() {
-    
+
+
     var isCharacterNumberOk = true;
     var numOfCharacter;
+    var counter = 0;
+
+    var isNan;
     while (isCharacterNumberOk) {
       numOfCharacter = prompt("How many characters would you like your password contain?");
-    
+
+
       if (parseInt(numOfCharacter) < 8 || parseInt(numOfCharacter) > 128) {
         alert("Choose character number between 8 and 128")
-      } else  isCharacterNumberOk = false;
+      } else {isCharacterNumberOk = false;}
+      
+      isNan=isNaN(numOfCharacter);
+      counter++
+      if (counter == 3) {
+        alert("You didn't enter any number.You are quitting pass-gen.")
+        isCharacterNumberOk = false;
+        isNan=false;
+      }
+      if (isNan) {
+        alert("Please enter a number between 8 and 128"); isCharacterNumberOk = true
+      }
+       
+      
     }
-    
 
-    if(numOfCharacter==null){
+
+    if (numOfCharacter == null) {
       alert("You didn't choose any options.You are quitting pass-gen.")
     }
 
     var typeOfCharacters = [];
-var count=0;
-var isGenerate;
-    while(! (isSpecial || isNumeric||isUppercase || isLowercase || isGenerate||  (numOfCharacter==null)) ){
+    var count = 0;
+    var isGenerate;
+    while (!( isSpecial || isNumeric || isUppercase || isLowercase || isGenerate || (numOfCharacter == null) || (isNaN(numOfCharacter)))) {
       alert("Please choose an option to generate password.")
-      
-    var isSpecial = confirm("Click ok to confirm including 'special' characters");
-    var isNumeric = confirm("Click ok to confirm including 'numeric' characters ");
-    var isUppercase = confirm("Click ok to confirm including 'UPPERCASE' characters ");
-    var isLowercase = confirm("Click ok to confirm including 'lowercase' characters ");
-    count++;
-    if(count==2){
-       isGenerate=true;
-       alert("You didn't choose any options.You are quitting pass-gen.")
-    }
 
-  }
+      var isSpecial = confirm("Click ok to confirm including 'special' characters");
+      var isNumeric = confirm("Click ok to confirm including 'numeric' characters ");
+      var isUppercase = confirm("Click ok to confirm including 'UPPERCASE' characters ");
+      var isLowercase = confirm("Click ok to confirm including 'lowercase' characters ");
+      count++;
+      if (count == 2) {
+        isGenerate = true;
+        alert("You didn't choose any options.You are quitting pass-gen.")
+      }
+
+    }
     var upperCaseCharacters = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H'];
     var lowerCaseCharacters = ['a', 'b', 'c', 'd', 'e', 'f', 'g'];
     var numericCharacters = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
@@ -60,10 +78,10 @@ var isGenerate;
     if (isLowercase) {
       typeOfCharacters.push(lowerCaseCharacters);
     }
-  
-    // alert(typeOfCharacters);
 
-     passW = new Array(numOfCharacter);
+    if((isNaN(numOfCharacter))){numOfCharacter=""};
+
+    passW = new Array(numOfCharacter);
     var lengthOfTypeOfCharacter = typeOfCharacters.length;
 
     var characterChosen = [];
@@ -71,21 +89,18 @@ var isGenerate;
 
       characterChosen = typeOfCharacters[i % lengthOfTypeOfCharacter];
 
-      //  alert("chosen" + characterChosen);
-
-
       var num = Math.floor(Math.random() * characterChosen.length);
 
       passW[i] = characterChosen[num];
 
     }
-    
+
   }
-   
 
 
-   str=passW.join("");
-passwordText.value =str;
+
+  str = passW.join("");
+  passwordText.value = str;
 
 }
 
